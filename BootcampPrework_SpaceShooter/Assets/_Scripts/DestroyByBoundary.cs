0ec8b1af47c58a654bc8438c319ea76a52830227
Emily Nelson <emily@fancrater.com>
2018-01-12 03:09:15 -0500

    SpaceShooter Prework

---
+++ BootcampPrework_SpaceShooter/Assets/_Scripts/DestroyByBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+	void OnTriggerExit (Collider other) 
+	{
+		Destroy(other.gameObject);
+	}
+}
---
+++ BootcampPrework_SpaceShooter/Assets/_Scripts/GameController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	public Text scoreText;
+	public Text gameOverText;
+	public Text winText;
+
+	private bool gameOver;
+	private int score;
+
+	void Start ()
+	{
+		gameOver = false;
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+		winText.text = "";
+	}
+	IEnumerator SpawnWaves () 
+	{	
+		
+		while (true)
+		{
+			yield return new WaitForSeconds (startWait);
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+
+			if (score == 100) {
+				Win ();
+				break;
+			}
+
+			if (gameOver) {
+				break;
+			}
+		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over :(";
+		gameOver = true;
+	}
+	public void Win ()
+	{
+		winText.text = "You Win :D";
+	}
+}
